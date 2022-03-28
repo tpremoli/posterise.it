cdd46c0560b1e29408b040e3f0cbfc164f376aa1
tpremoli <tomas@premoli.org>
2022-03-29 00:43:31 +0100

    Began implementing spotify auth

--- Spotifier/polaroidizer/urls.py
+++ Spotifier/polaroidizer/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
 from . import views
+from .views import AuthURL, is_authenticated, spotify_callback
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', AuthURL.as_view(), name='login'),
+    path('redirect/', spotify_callback, name='redirect'),
+    path('is-authenticated/', is_authenticated.as_view(), name='is_authenticated'),
 ]
