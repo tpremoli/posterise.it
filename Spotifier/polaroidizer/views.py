89427e9038cc400d11d5f78a91251707c8b6be7f
tpremoli <tomas@premoli.org>
2022-03-28 23:57:49 +0100

    Cleanup

--- Spotifier/polaroidizer/views.py
+++ Spotifier/polaroidizer/views.py
@@ -1,6 +1,14 @@
+from __future__ import print_function
 from django.shortcuts import render
+import random
+import spotipy
 
 
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+
+
+def login(request):
+    context = {}
+    return render(request, 'login.html', context)
