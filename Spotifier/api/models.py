2007dca16b00f2822c6fc1450ec1f7b49ed984b5
tpremoli <tomas@premoli.org>
2022-03-29 15:36:42 +0100

    Started creating CreatePolaroid frontend form

--- Spotifier/api/models.py
+++ Spotifier/api/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Polaroid(models.Model):
     creator = models.CharField(max_length=50)
-    track_uri = models.CharField(max_length=50, unique=True)
-    track_name = models.CharField(max_length=50)
-    track_artist = models.CharField(max_length=50)
-    track_length = models.TimeField()
+    is_album = models.BooleanField(default=False)
+
+    uri = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    length = models.TimeField()
--- Spotifier/frontend/src/components/CreatePolaroid.js
+++ Spotifier/frontend/src/components/CreatePolaroid.js
@@ -1,4 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Checkbox from '@material-ui/core/Checkbox';
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreatePolaroid extends Component {
     constructor(props) {
@@ -7,13 +18,13 @@ export default class CreatePolaroid extends Component {
             spotifyAuthenticated: false,
         }
         this.authenticateSpotify = this.authenticateSpotify.bind(this);
-        this.authenticateSpotify();
+        // this.authenticateSpotify();
     }
 
     authenticateSpotify() {
         fetch("/polaroidizer/is-authenticated").then(res => res.text())
             .then(text => console.log(text));
-        
+
         fetch("/polaroidizer/is-authenticated")
             .then((response) => response.json())
             .then((data) => {
@@ -30,6 +41,44 @@ export default class CreatePolaroid extends Component {
     }
 
     render() {
-        return <p> this is the create polaroid page </p>;
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography component="h4" variant="h4">
+                        Create A Polaroid!
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl component="fieldset">
+                        <FormHelperText>
+                            <div align="center">
+                                Options
+                            </div>
+                        </FormHelperText>
+                        <FormControlLabel control={<Checkbox defaultChecked />} label="Include Track Length" />
+                        <FormControlLabel control={<Checkbox defaultChecked />} label="Include Artist" />
+                        <TextField id="standard-basic" label="URI" variant="standard" />
+                        <RadioGroup
+                            row
+                            defaultValue="false"
+                            onChange={this.handleGuestCanPauseChange}
+                        >
+                            <FormControlLabel
+                                value="true"
+                                control={<Radio color="primary" />}
+                                label="Album"
+                                labelPlacement="bottom"
+                            />
+                            <FormControlLabel
+                                value="false"
+                                control={<Radio color="secondary" />}
+                                label="Track"
+                                labelPlacement="bottom"
+                            />
+                        </RadioGroup>
+                    </FormControl>
+                </Grid>
+            </Grid>
+        );
     }
 }
