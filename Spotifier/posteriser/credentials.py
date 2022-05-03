3545fffe400436509b2d232bb00bca0ba1d7ffec
tpremoli <tomas@premoli.org>
2022-05-03 16:03:07 +0100

    Obscured Client ID and Secret

--- Spotifier/posteriser/credentials.py
+++ Spotifier/posteriser/credentials.py
@@ -1,5 +1,5 @@
-CLIENT_ID = "eef79e1608c74f3ea9325791936d59ec"
-CLIENT_SECRET = "bc5dd71580e4436ba7ae10994969fd3e"
+CLIENT_ID = "{spotify client ID here}"
+CLIENT_SECRET = "{spotify client secret here}"
 REDIRECT_URI = "http://posterise.it/api/redirect"
 # REDIRECT_URI = "http://127.0.0.1:8000/redirect"
 # For testing ^^
